03370450273cd181552a8f5c41bb63f7e80bb258
Roberto-Valdez <43326202+Roberto-Valdez@users.noreply.github.com>
2020-04-11 15:02:21 -0400

    Update MainActivity.java

--- app/src/main/java/edu/uga/cs/project4_geography_quiz/MainActivity.java
+++ app/src/main/java/edu/uga/cs/project4_geography_quiz/MainActivity.java
@@ -1,16 +1,25 @@
 package edu.uga.cs.project4_geography_quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /* This activity is for the splash screen that displays at launch. It sets listeners for the two buttons. */
 public class MainActivity extends AppCompatActivity {
 
     private Button start_btn;
     private Button results_btn;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +31,13 @@ public class MainActivity extends AppCompatActivity {
 
         start_btn.setOnClickListener( new StartButtonClickListener() );
         results_btn.setOnClickListener( new ResultsButtonClickListener() );
+
+        GeoQuizData db = new GeoQuizData(context);
+        db.open();
+        db.close();
+
     }
+
 
     private class StartButtonClickListener implements View.OnClickListener {
         @Override
