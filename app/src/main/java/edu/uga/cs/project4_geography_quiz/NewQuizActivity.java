a1d12a7cd2aadd10bf8dcb93fd54eec0f820c3b8
kevinwebb <kaw51647@uga.edu>
2020-04-11 22:40:53 -0400

    added swipe left functionality

--- app/src/main/java/edu/uga/cs/project4_geography_quiz/NewQuizActivity.java
+++ app/src/main/java/edu/uga/cs/project4_geography_quiz/NewQuizActivity.java
@@ -1,17 +1,21 @@
 package edu.uga.cs.project4_geography_quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class NewQuizActivity extends AppCompatActivity {
+public class NewQuizActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
     private String countryXString, option1String, option2String, option3String;
     private TextView textView1;
@@ -20,6 +24,9 @@ public class NewQuizActivity extends AppCompatActivity {
     public Float quizResult;
     public List<Question> quizQuestions;
     public List<Country> quizCountries;
+    private GestureDetectorCompat detector;
+    private float x1,x2;
+    static final int MIN_DISTANCE = 150;
 
     //public GeoQuizData data;
 
@@ -36,6 +43,8 @@ public class NewQuizActivity extends AppCompatActivity {
         radio1 = findViewById( R.id.radioButton1 );
         radio2 = findViewById( R.id.radioButton2 );
         radio3 = findViewById( R.id.radioButton3 );
+
+        detector = new GestureDetectorCompat(this, this);
 
 
         quizDate = new Date();                        //save the date for the current quiz
@@ -55,9 +64,11 @@ public class NewQuizActivity extends AppCompatActivity {
 
 
 
+
+
+
         
 
-        //load in String values
 
         //quizQuestions.add(new Question("1", "2", "3", quizCountries.get(i)));
 
@@ -84,15 +95,13 @@ public class NewQuizActivity extends AppCompatActivity {
 
 
 
-        Country c1 = new Country("France", "Europe");
-
-        Question q1 = new Question("Europe", "GEorgia", "fff", c1);
-       // q1.countryX.setCountryName("France");
+         Country c1 = new Country("France", "Europe");
+         Question q1 = new Question("Europe", "Georgia", "fff", c1);
+         q1.countryX.setCountryName("France");
         //q1.countryX.setContinent("Europe");
+        // textView1.setText(q1.countryX.getCountryName());
+        //  radio1.setText(q1.countryX.getContinent());
 
-
-         textView1.setText(q1.countryX.getCountryName());
-         radio1.setText(q1.countryX.getContinent());
 
 
         //quizQuestions.get(0).countryX.setCountryName("France");
@@ -110,6 +119,7 @@ public class NewQuizActivity extends AppCompatActivity {
 
     }
 
+
     /*public void populateCountries() {
         //GeoQuizData data
         data.open();
@@ -117,5 +127,55 @@ public class NewQuizActivity extends AppCompatActivity {
 
     }*/
 
+    /*public Boolean checkAnswer() {
 
+    }*/
+
+
+
+
+    //for detecting swipe left
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        detector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+
+    //detect left swipe;
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        if (e1.getX() > e2.getX()) {
+            radio1.setText("Swiped");
+
+
+        }
+            return true;
+    }
 }
